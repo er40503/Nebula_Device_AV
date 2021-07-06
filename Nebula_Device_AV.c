1db062169b7226166b70161638aee8e2733a3b6f
kyk <38483591+er40503@users.noreply.github.com>
2021-07-06 19:42:37 +0800

    Add files via upload

--- Nebula_Device_AV.c
+++ Nebula_Device_AV.c
@@ -480,6 +480,26 @@ int StartAvServer(int sid, unsigned char chid, unsigned int timeout_sec, unsigne
 static void *ThreadVideoFrameData(void *arg) {
 
 #define BUFSIZE 16*1024*1024
+    /*==========================================================================*/
+    // H.264 Slice_Splitting Testing
+    uint8_t *buf = (uint8_t *) malloc(BUFSIZE);
+    h264_stream_t *h = h264_new();
+
+    /*=================================malloc=========================================*/
+    FILE *infile = fopen(gLiveIframePath, "rb");
+    if (infile == NULL) {
+        fprintf(stderr, "!! Error: could not open file: %s \n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    char fname_buf[1024] = {0};
+    memset(fname_buf, 0, 1024);
+    sprintf(fname_buf, "%s.misc", argv[1]);
+    FILE *outfile_misc = fopen(fname_buf, "wb");
+    if (outfile_misc == NULL) {
+        fprintf(stderr, "!! Error: could not open file: %s \n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
 
     unsigned int total_count = 0;
     float hF = 0.0, lF = 0.0, total_fps = 0;
@@ -513,9 +533,8 @@ static void *ThreadVideoFrameData(void *arg) {
 //    int nalu_size = 0;
 
     unsigned long diff;
-    int key = 0;
+    int flag = 0;
 
-    int ericnt = 0;
 
     /*==========================================================================*/
     size_t rsz = 0;
@@ -524,11 +543,11 @@ static void *ThreadVideoFrameData(void *arg) {
     uint8_t *p = buf;
     int nal_start, nal_end;
     //this is to identify whether pps is written or not
-    char *pps_buf[32];
-    int pps_buf_size[32];
-    int cnt = 0;
     char *head;
     int total_byte = 0;
+    int cnt = 0;
+
+
     FILE *fp;
     char str[50];
     int freadtime = 0;
@@ -547,13 +566,10 @@ static void *ThreadVideoFrameData(void *arg) {
         printf("%s: exit\n", __func__);
         pthread_exit(0);
     }*/
-    /*==========================================================================*/
-    FILE *infile = fopen(gLiveIframePath, "rb");
-    if (infile == NULL) {
-        fprintf(stderr, "!! Error: could not open file: %s \n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    if (h264_dbgfile == NULL) { h264_dbgfile = stdout; }
+
+
+
+//    if (h264_dbgfile == NULL) { h264_dbgfile = stdout; }
 
 
     /* Get the size of the file. */
@@ -567,51 +583,28 @@ static void *ThreadVideoFrameData(void *arg) {
     fsize = s.st_size;
     printf("fsize = %d\n", fsize);
     // nalu = malloc(fsize);
-    nalu = malloc(102400);
+//    nalu = malloc(102400);
 
     printf("%s start OK\n", __func__);
     printf("[Video] is ENABLED!!\n");
 
-    /*==========================================================================*/
-    // H.264 Slice_Splitting Testing
-    uint8_t *buf = (uint8_t *) malloc(BUFSIZE);
-    h264_stream_t *h = h264_new();
 
     while (gProgressRun) {
-        freadtime += 1;
-        sz = 0;
         rsz = fread(buf + sz, 1, BUFSIZE - sz, infile);
-        printf("read time = %d\n", freadtime);
         if (rsz == 0) {
             if (ferror(infile)) {
                 fprintf(stderr, "!! Error: read failed: %s \n", strerror(errno));
                 break;
             }
-            // Init Params Again
+            fclose(infile);
             memset(buf, 0, BUFSIZE);
-            infile = fopen(argv[1], "rb"); // Read New File
+            infile = fopen(argv[1], "rb");
             sz = 0;
-            off = 0;
             p = buf;
             nal_start = 0;
             nal_end = 0;
-            memset(pps_buf, 0, 32);
-            memset(pps_buf_size, 0, 32);
             cnt = 0;
-            total_byte = 0;
-
-            // File Access
-            fclose(infile);
-            char wfile_name[50];
-            ericnt += 1;
-            sprintf(wfile_name, "/mnt/sdcard/mpi_test/PBmmap_.%d", ericnt);
-            infile = fopen(wfile_name, "rb");
-            if (infile == NULL) {
-                fprintf(stderr, "!! Error: could not open file: %s \n", strerror(errno));
-                exit(EXIT_FAILURE);
-            }
-
-            continue; // GoTo Top
+            continue;
 
             //break;  // if (feof(infile))
         }
@@ -619,133 +612,51 @@ static void *ThreadVideoFrameData(void *arg) {
         sz += rsz;
 
         while (find_nal_unit(p, sz, &nal_start, &nal_end) > 0) {
-            fprintf(h264_dbgfile, "!! Found NAL at offset %lld (0x%04llX), size %lld (0x%04llX) \n",
-                    (long long int) (off + (p - buf) + nal_start),
-                    (long long int) (off + (p - buf) + nal_start),
-                    (long long int) (nal_end - nal_start),
-                    (long long int) (nal_end - nal_start));
-
-            fprintf(h264_dbgfile, "XX ");
-            debug_bytes(p, nal_end - nal_start >= 16 ? 16 : nal_end - nal_start);
-
             p += nal_start;
             read_debug_nal_unit(h, p, nal_end - nal_start);
 
             //check nal type
             switch (h->nal->nal_unit_type) {
-                case NAL_UNIT_TYPE_CODED_SLICE_IDR:
                 case NAL_UNIT_TYPE_CODED_SLICE_NON_IDR:
-                case NAL_UNIT_TYPE_CODED_SLICE_AUX:
-                    printf("reference pps: %d & sps: %d\n", h->sh->pic_parameter_set_id,
-                           h->pps_table[h->sh->pic_parameter_set_id]->seq_parameter_set_id);
-
-                    if (pps_buf[h->sh->pic_parameter_set_id] != NULL) {
-                        fwrite(pps_buf[h->sh->pic_parameter_set_id], 1, pps_buf_size[h->sh->pic_parameter_set_id],
-                               outfile_base);
-                        free(pps_buf[h->sh->pic_parameter_set_id]);
-                        pps_buf[h->sh->pic_parameter_set_id] = NULL;
-                    }
-
-                    //start saving the slices
-                    fwrite(p - nal_start, 1, nal_end, outfile_base);
-
+                    printf("NAL_UNIT_TYPE_CODED_SLICE_NON_IDR\n");
+//                    memset(fname_buf, 0, 1024);
+//                    sprintf(fname_buf, "file.%d", cnt);
+//                    outfile_misc = fopen(fname_buf, "wb");
+//                    fwrite(p - nal_start, 1, nal_end, outfile_misc);
+//                    fclose(outfile_misc);
+//                    cnt += 1;
+                    nalu = p - nal_start;
+                    nalu_size = nal_end;
+                    flag == 1
                     break;
-
+                case NAL_UNIT_TYPE_CODED_SLICE_IDR:
+                    printf("NAL_UNIT_TYPE_CODED_SLICE_IDR\n");
+//                    total_byte += nal_end;
+//                    memset(fname_buf, 0, 1024);
+//                    sprintf(fname_buf, "file.%d", cnt);
+//                    outfile_misc = fopen(fname_buf, "wb");
+//                    fwrite(head, 1, total_byte, outfile_misc);
+//                    fclose(outfile_misc);
+//                    cnt += 1;
+                    nalu = head;
+                    nalu_size = total_byte;
+                    flag == 1
+                    break;
                 case NAL_UNIT_TYPE_SPS:
-                    fwrite(p - nal_start, 1, nal_end, outfile_base);
+                    printf("NAL_UNIT_TYPE_SPS\n");
+                    head = p - nal_start;
+                    total_byte = 0;
+                    total_byte += nal_end;
                     break;
-
-                case NAL_UNIT_TYPE_PPS:
-                    pps_buf[h->pps->pic_parameter_set_id] = malloc(nal_end);
-                    memcpy(pps_buf[h->pps->pic_parameter_set_id], p - nal_start, nal_end);
-                    pps_buf_size[h->pps->pic_parameter_set_id] = nal_end;
-
-                    break;
-
-                    //SVC support
-                case NAL_UNIT_TYPE_SUBSET_SPS:
-                    printf("sps_ext id: %d\n", h->sps_subset->sps->seq_parameter_set_id);
-                    memset(fname_buf, 0, 1024);
-                    sprintf(fname_buf, "%s.l_%d", argv[1], h->sps_subset->sps->seq_parameter_set_id);
-                    outfile_layers[h->sps_subset->sps->seq_parameter_set_id] = fopen(fname_buf, "wb");
-                    if (outfile_layers[h->sps_subset->sps->seq_parameter_set_id] == NULL) {
-                        fprintf(stderr, "!! Error: could not open file: %s \n", strerror(errno));
-                        exit(EXIT_FAILURE);
-                    }
-
-                    fwrite(p - nal_start, 1, nal_end, outfile_layers[h->sps_subset->sps->seq_parameter_set_id]);
-                    break;
-
-                    //SVC support
-                case NAL_UNIT_TYPE_CODED_SLICE_SVC_EXTENSION:
-                    printf("reference extension pps: %d & sps: %d\n", h->sh->pic_parameter_set_id,
-                           h->pps_table[h->sh->pic_parameter_set_id]->seq_parameter_set_id);
-
-                    if (pps_buf[h->sh->pic_parameter_set_id] != NULL) {
-                        fwrite(pps_buf[h->sh->pic_parameter_set_id], 1, pps_buf_size[h->sh->pic_parameter_set_id],
-                               outfile_layers[h->pps_table[h->sh->pic_parameter_set_id]->seq_parameter_set_id]);
-                        free(pps_buf[h->sh->pic_parameter_set_id]);
-                        pps_buf[h->sh->pic_parameter_set_id] = NULL;
-                    }
-
-                    //start saving the slices
-                    fwrite(p - nal_start, 1, nal_end,
-                           outfile_layers[h->pps_table[h->sh->pic_parameter_set_id]->seq_parameter_set_id]);
-                    break;
-
                 default:
-                    fwrite(p - nal_start, 1, nal_end, outfile_misc);
+                    printf("default\n");
+                    total_byte += nal_end;
+                    //fwrite(p - nal_start, 1, nal_end, outfile_misc);
                     break;
             }
 
             //save nal to corresponding file
-            if (h->nal->nal_unit_type == NAL_UNIT_TYPE_SPS) {
-                head = p - nal_start;
-                total_byte = 0;
-            }
-            total_byte += nal_end;
-
-            if (h->nal->nal_unit_type == NAL_UNIT_TYPE_CODED_SLICE_NON_IDR) {
-//                sprintf(str, "file.%d", cnt);
-//                fp = fopen(str, "wb");
-//                if (fp == NULL) {
-//                    printf("Error!");
-//                }
-//                printf("size = %d\n", nal_end);
-//                fwrite(p - nal_start, 1, nal_end, fp);
-//                cnt += 1;
-//                fclose(fp);
-
-                key = 1;
-                nalu = p - nal_start;
-                nalu_size = nal_end;
-                //TODO Starting with PB-Frame
-                memset(&frame_info, 0, sizeof(frame_info));
-                frame_info.codec_id = MEDIA_CODEC_VIDEO_H264;
-                frame_info.flags = IPC_FRAME_FLAG_PBFRAME;
-            }
-            else if (h->nal->nal_unit_type == NAL_UNIT_TYPE_CODED_SLICE_IDR) {
-//                sprintf(str, "file.%d", cnt);
-//                fp = fopen(str, "wb");
-//                if (fp == NULL) {
-//                    printf("Error!");
-//                }
-//                printf("size = %d\n", total_byte);
-//                fwrite(head, 1, total_byte, fp);
-//                cnt += 1;
-//                fclose(fp);
-
-                key = 1;
-                nalu = head;
-                nalu_size = total_byte;
-                //TODO Starting with IDR-Frame
-                memset(&frame_info, 0, sizeof(frame_info));
-                frame_info.codec_id = MEDIA_CODEC_VIDEO_H264;
-                frame_info.flags = IPC_FRAME_FLAG_IFRAME;
-            }
-            else;
-
-            if (key == 1) {
+            if (flag == 1) {
                 //TODO TUTK starting with here...
                 frame_info.timestamp = GetTimeStampMs();
                 send_frame_out = 0;
@@ -838,7 +749,7 @@ static void *ThreadVideoFrameData(void *arg) {
                     usleep(sleep_us - send_frame_us);
                 }
 
-                key = 0;
+                flag = 0;
             }
 
             //skip to next NAL
@@ -867,8 +778,8 @@ static void *ThreadVideoFrameData(void *arg) {
     h264_free(h);
     free(buf);
 
-    fclose(h264_dbgfile);
     fclose(infile);
+    fclose(outfile_misc);
 
     printf("[%s] exit High/Low [%f/%f] AVG[%f] totalCnt[%d]\n", __func__, hF, lF, (float) total_fps / round,
            total_count);
